e442988ea88aba30ad49c20c87bbbe144b5114a7
Amira Elmansy <amira.elmansy@gulfsip.com>
2014-07-18 23:45:56 +0200

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Seal.h
@@ -0,0 +1,13 @@
+//
+//  Seal.h
+//  PeevedPenguins
+//
+//  Created by Amira Elmansy on 7/18/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Seal : CCSprite
+
+@end
---
+++ PeevedPenguins.spritebuilder/Source/Seal.m
@@ -0,0 +1,24 @@
+//
+//  Seal.m
+//  PeevedPenguins
+//
+//  Created by Amira Elmansy on 7/18/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Seal.h"
+
+@implementation Seal
+
+-(id)init
+{
+    self = [super init];
+    if (self)
+    {
+        CCLOG(@"Seal Created");
+    }
+    
+    return self;
+}
+
+@end
